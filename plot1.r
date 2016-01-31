26364e2d0f8215697daaf470d4ef1ea7171a127b
ravigautam002 <ravigautam002@gmail.com>
2016-01-31 22:49:33 +0100

    modif

--- plot1.r
+++ plot1.r
@@ -1,3 +1,6 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
 total<-with(NEI, tapply(Emissions, year, sum))
 png('plot1.png')
 plot(names(total), total, ylab='Emissions', xlab='year', main='United States')
